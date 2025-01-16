87b20d49ddea7455da5e0db681c88ebc53378e78
Alessandro Zamboni <alessandro.zamboni@avanade.com>
2025-01-16 09:26:15 +0100

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,12 @@
+function skillsMember() {
+  return {
+            id: 'skills-copilot-codespaces-vscode',
+            name: 'Skills Copilot Codespaces',
+            description: 'A collection of skills for Codespaces',
+            icon: 'https://avatars.githubusercontent.com/u/89188619?s=200&v=4',
+            member: {
+            username: 'skills-copilot-codespaces',
+            profileUrl: ''
+        }
+    }
+}
